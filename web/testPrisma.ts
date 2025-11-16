c58830cbce54d4f20387f0f008dc1aaa7e665c20
alessandroamormino <amormino.alessandro97@gmail.com>
2025-11-16 10:46:42 +0100

    test

--- web/testPrisma.ts
+++ web/testPrisma.ts
@@ -1,14 +1,10 @@
-import * as dotenv from 'dotenv';
-dotenv.config();  // legge .env nella root del progetto
-
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+import { prisma } from './lib/prisma';
 
 async function main() {
-  const episodes = await prisma.episodes.findMany();
-  console.log(episodes);
+  const c = await prisma.case.findMany({ include: { episodes: true } });
+  console.log(c);
 }
 
 main()
   .catch(e => console.error(e))
-  .finally(async () => await prisma.$disconnect());
+  .finally(() => prisma.$disconnect());
