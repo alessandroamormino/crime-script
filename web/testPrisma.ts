118a9eed2d3ae3516ab8e33a8fe61a692ab97b23
alessandroamormino <amormino.alessandro97@gmail.com>
2025-11-16 10:11:12 +0100

    test: prisma connection

---
+++ web/testPrisma.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const users = await prisma.users.findMany();
+  console.log(users);
+}
+
+main()
+  .catch(e => {
+    console.error(e);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
